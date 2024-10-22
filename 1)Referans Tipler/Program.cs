93d4eb5ae089279a521ca5b40c138f8de87db26f
ali.hazar <ali.hazar@commercelab.com.tr>
2024-10-22 17:44:37 +0300

    Add project files.

---
+++ 1)Referans Tipler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1_Referans_Tipler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product {Id=1,Name="Table",Stock=55};
+
+            ValueorReference valueorReference= new ValueorReference();
+            valueorReference.Show(product1);
+
+            Console.WriteLine(product1.Name);
+            Console.ReadLine();
+            
+            
+
+
+        }
+
+    }
+}
---
+++ 1)Referans Tipler/ValueorReference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Referans_Tipler
+{
+    public class ValueorReference
+    {
+        public void Show(Product product)
+        {
+            product.Name="Computer";
+        }
+    }
+}
